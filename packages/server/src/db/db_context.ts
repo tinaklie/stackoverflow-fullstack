39352a44e5c95ba02351ad0e83a1c2d46f011078
Tina Klie <tina.klie@exxeta.com>
2023-12-12 16:17:37 +0100

    add "fake data" to db

--- packages/server/src/db/db_context.ts
+++ packages/server/src/db/db_context.ts
@@ -1,14 +1,23 @@
 import { MongoClient } from "mongodb";
-import { AnswerDB, QuestionDB } from "./types";
+import { QuestionDB } from "./types";
 
 export async function connectToDb() {
-  console.log("process.env", process.env.MONGO_URL);
   const client = await MongoClient.connect(process.env.MONGO_URL ?? "");
   const db = client.db(process.env.MONGO_DATABASE ?? "questions-sample");
+
+  // const inserted = await db.collection<QuestionDB>("questions").insertOne({
+  //   _id: new ObjectId(),
+  //   title: "How to insert a question into MongoDB?",
+  //   description: "I want to insert a Question into MongoDB - How to do it?",
+  //   answers: [],
+  //   comments: [],
+  //   votes: 0,
+  // });
+
+  // console.log(inserted.insertedId);
   return {
     db,
     questions: db.collection<QuestionDB>("questions"),
-    answers: db.collection<AnswerDB>("answers"),
   };
 }
 
