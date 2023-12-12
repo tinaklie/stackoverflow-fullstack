de39d3d2e94958ff89d4a26390b4374bc0d83270
Tina Klie <tina.klie@exxeta.com>
2023-12-12 11:04:04 +0100

    add apolloprovider to app

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { routes } from "./pages/routes";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./gql/client";
 
 const router = createBrowserRouter(routes);
 
-function App() {
-  return <RouterProvider router={router} />;
+export function App() {
+  return (
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
+  );
 }
-
-export default App;
