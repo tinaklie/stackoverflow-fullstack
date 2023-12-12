6949b2252521f621e02bea1d2613413faa34d30d
Tina Klie <tina.klie@exxeta.com>
2023-12-12 16:12:52 +0100

    add resolvers to createSchema in main

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -4,12 +4,14 @@ import { createServer } from "http";
 import * as dotenv from "dotenv";
 import { GqlContext, createContext } from "./create-context";
 import { typeDefinitions } from "./schema";
+import { resolvers } from "./resolver";
 
 dotenv.config();
 
 async function main() {
   const schema = createSchema<GqlContext>({
     typeDefs: typeDefinitions,
+    resolvers,
   });
   const context = await createContext();
   const yoga = createYoga({ schema, context, renderGraphiQL });
