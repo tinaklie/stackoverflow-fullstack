2ad69a4632066f79252f7e9c74cc1db4f22c67e8
tinaklie <tina.klie@exxeta.com>
2023-12-15 21:22:23 +0100

    save new Question

--- packages/client/src/question-create/CreateQuestionPage.tsx
+++ packages/client/src/question-create/CreateQuestionPage.tsx
@@ -1,14 +1,41 @@
 import { FormProvider, useForm } from "react-hook-form";
 import "./CreateQuestionPage.css";
+import { graphql } from "../gql";
+import { useMutation } from "@apollo/client";
+import { useNavigate } from "react-router-dom";
+
+const saveQuestionDocument = graphql(/* GraphQL */ `
+  mutation saveQuestion($question: QuestionInput!) {
+    saveQuestion(question: $question)
+  }
+`);
+
+type FormData = {
+  title: string;
+  description: string;
+};
 
 export const CreateQuestionPage: React.FC = () => {
   const formMethods = useForm({ values: { title: "", description: "" } });
   const {
+    register,
     formState: { isDirty },
   } = formMethods;
 
-  function saveQuestion() {
-    // TODO
+  const navigate = useNavigate();
+
+  const [saveNewQuestion] = useMutation(saveQuestionDocument);
+
+  function saveQuestion(data: FormData) {
+    return saveNewQuestion({
+      variables: {
+        question: {
+          title: data.title,
+          description: data.description,
+        },
+      },
+      onCompleted: () => navigate("/"),
+    });
   }
   return (
     <div className="editor-main">
@@ -18,13 +45,27 @@ export const CreateQuestionPage: React.FC = () => {
         <form onSubmit={formMethods.handleSubmit(saveQuestion)}>
           <div>
             <div>Title</div>
-            <input id="title" type="text" className="title-input" />
+            <input
+              id="title"
+              type="text"
+              className="title-input"
+              {...register("title")}
+            />
           </div>
           <div className="description">
             <div>Description</div>
-            <textarea id="title" className="title-input" rows={15} />
+            <textarea
+              id="title"
+              className="title-input"
+              rows={15}
+              {...register("description")}
+            />
           </div>
-          <button type="submit" disabled={!isDirty} className="add-question-button">
+          <button
+            type="submit"
+            disabled={!isDirty}
+            className="add-question-button"
+          >
             Save Question
           </button>
         </form>
